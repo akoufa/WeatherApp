2afa07ea5312e1ceb594bb05c2b9ac77d96ff4c9
Alexandros Koufatzis <alexk@Alexandross-MacBook-Pro.local>
2016-11-23 14:18:00 +0100

    Replaced custom bottom navigation with the design support lib one

--- app/src/main/java/com/akoufatzis/weatherapp/cityweathermain/CityWeatherMainActivity.java
+++ app/src/main/java/com/akoufatzis/weatherapp/cityweathermain/CityWeatherMainActivity.java
@@ -1,39 +1,33 @@
 package com.akoufatzis.weatherapp.cityweathermain;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
-import android.view.Gravity;
-import android.view.ViewGroup;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.akoufatzis.weatherapp.R;
 import com.akoufatzis.weatherapp.cityweatherfavorite.view.CityWeatherFavoriteFragment;
 import com.akoufatzis.weatherapp.cityweathersearch.view.CityWeatherSearchFragment;
 import com.akoufatzis.weatherapp.common.BaseToolbarActivity;
-import com.akoufatzis.weatherapp.widgets.BottomBarItem;
-import com.akoufatzis.weatherapp.widgets.BottomBarLayout;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
-
-import static com.akoufatzis.weatherapp.utilities.ViewUtils.dp2px;
 
 /**
  * Created by alexk on 02/05/16.
  */
-public class CityWeatherMainActivity extends BaseToolbarActivity implements BottomBarLayout.OnBottomBarItemClickListener {
+public class CityWeatherMainActivity extends BaseToolbarActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private static final String BOTTOM_BAR_TAG = "bottom_bar_tag";
 
     @BindView(R.id.rootCoordinatorLayout)
-    public CoordinatorLayout coordinatorLayout;
-    protected BottomBarLayout bottomBarLayout;
-    private int currentIndex = -1;
+    CoordinatorLayout coordinatorLayout;
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected int getLayoutResourceId() {
@@ -43,79 +37,42 @@ public class CityWeatherMainActivity extends BaseToolbarActivity implements Bott
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        List<BottomBarItem> bottomBarItems = new ArrayList<>(3);
-        bottomBarItems.add(new BottomBarItem(this,
-                new BottomBarItem.BottomBarItemOption(R.drawable.ic_search_white_24dp,
-                        R.string.search, android.R.color.white, 0)));
-        bottomBarItems.add(new BottomBarItem(this,
-                new BottomBarItem.BottomBarItemOption(R.drawable.ic_favorite_white_24dp,
-                        R.string.favorites, android.R.color.white, 1)));
-        bottomBarItems.add(new BottomBarItem(this,
-                new BottomBarItem.BottomBarItemOption(R.drawable.ic_gps_fixed_white_24dp,
-                        R.string.nearby, android.R.color.white, 2)));
-        bottomBarLayout = new BottomBarLayout(this, bottomBarItems, 0);
-        bottomBarLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.primary_dark));
-
-        CoordinatorLayout.LayoutParams params
-                = new CoordinatorLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dp2px(56));
-        params.gravity = Gravity.BOTTOM;
-        coordinatorLayout.addView(bottomBarLayout, params);
-
-        bottomBarLayout.setOnBottomBarItemClickListener(this);
-
-        //Initial State
-        onBottomBarItemClicked(bottomBarItems.get(0));
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
     protected void onDestroy() {
-        bottomBarLayout.setOnBottomBarItemClickListener(null);
+        bottomNavigationView.setOnNavigationItemSelectedListener(null);
         super.onDestroy();
     }
 
     @Override
-    public void onBottomBarItemClicked(BottomBarItem bottomBarItem) {
-
-        // Already shown
-        if (currentIndex == bottomBarItem.getIndex()) {
-
-            return;
-        }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment;
 
-        Fragment fragment = getFragmentForIndex(bottomBarItem.getIndex());
+        switch (item.getItemId()) {
+
+            case R.id.action_search:
+                fragment = CityWeatherSearchFragment.newInstance();
+                break;
+            case R.id.action_favorites:
+                fragment = CityWeatherFavoriteFragment.newInstance();
+                break;
+            case R.id.action_nearby:
+                // TODO: Implement this feature
+                fragment = CityWeatherSearchFragment.newInstance();
+                Toast.makeText(this, "This feature is not yet implemented", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                fragment = CityWeatherSearchFragment.newInstance();
+                break;
+        }
 
         fragmentManager.beginTransaction().replace(R.id.bottom_bar_fragment_container,
                 fragment, BOTTOM_BAR_TAG).commit();
 
-    }
-
-    private Fragment getFragmentForIndex(int index) {
-
-        // TODO: Remove this when the other cases are implemented
-        Fragment fragment = CityWeatherSearchFragment.newInstance();
-
-        switch (index) {
-
-            case 0:
-                fragment = CityWeatherSearchFragment.newInstance();
-                currentIndex = 0;
-                break;
-
-            case 1:
-                fragment = CityWeatherFavoriteFragment.newInstance();
-                currentIndex = 1;
-                break;
-
-            case 2:
-                Toast.makeText(this, "This feature is not yet implemented", Toast.LENGTH_SHORT).show();
-//                fragment = CityWeatherSearchFragment.newInstance();
-                currentIndex = 2;
-                break;
-        }
-
-        return fragment;
+        return true;
     }
 }
